a7a9981990344ec0fc9a9ba4577dfd0b4f6897b3
Zoe Williams <flatironschool@Flatirons-MacBook-Air-6.local>
2018-09-05 14:47:00 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -27,11 +27,28 @@ class Song
         @@genres.uniq
     end
 
-    def genre_count
-        @@genres.uniq
-    end
+    def self.artist_count
+        artist_count = {}
+        @@artists.each do |artist|
+          if artist_count[artist]
+            artist_count[artist] += 1 
+          else
+            artist_count[artist] = 1
+          end
+        end
+        artist_count
+      end
 
-    def artist_count
-    end
+      def self.genre_count
+        genre_count = {}
+        @@genres.each do |genre|
+          if genre_count[genre]
+            genre_count[genre] += 1 
+          else
+            genre_count[genre] = 1
+          end
+        end
+        genre_count
+      end
 
 end
