f3fde24049663dfb6d11428d0d870e946bdf8e75
Zoe Williams <flatironschool@Flatirons-MacBook-Air-6.local>
2018-09-05 14:36:14 -0500

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,37 @@
+class Song
+
+    attr_accessor :name, :artist, :genre
+
+    @@count = 0
+    @@genres = []
+    @@artists = []
+
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count += 1
+        @@genres << genre
+        @@artists << artist
+    end
+
+    def self.count
+        @@count
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def genre_count
+        @@genres.uniq
+    end
+
+    def artist_count
+    end
+
+end
